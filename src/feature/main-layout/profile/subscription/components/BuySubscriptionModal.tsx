9857c35dd78780f915b5f56b4c30d3fd0784a0aa
MithilaKhan <mithilakhan082@gmail.com>
2026-02-03 17:02:27 +0600

    add service

--- src/feature/main-layout/profile/subscription/components/BuySubscriptionModal.tsx
+++ src/feature/main-layout/profile/subscription/components/BuySubscriptionModal.tsx
@@ -1,28 +1,30 @@
-import  { buySubscriptions } from "@/constants/subscription"
+import { buySubscriptions } from "@/constants/subscription"
 import { Modal } from "antd"
 import SubscriptionCard from "./SubscriptionCard"
+import { myFetch } from "../../../../../../helpers/myFetch";
 
-const BuySubscriptionModal = ({open, setOpen}: {open: boolean, setOpen: (open: boolean) => void}) => {
+const BuySubscriptionModal = async({ open, setOpen }: { open: boolean, setOpen: (open: boolean) => void }) => {
+
     return (
         <Modal open={open} onCancel={() => setOpen(false)} footer={null} centered width={"auto"}>
-           <div className="pb-10"> 
-            <h1 className="text-xl md:text-2xl font-medium text-[#292929] pb-2">Subscription Packages</h1> 
-            <p className="text-[#919191] text-sm font-medium">Flexible plans based on how many services you handle</p>
-           </div> 
-
-           <div> 
-            <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"> 
-                {
-                    buySubscriptions?.map((data: any) => ( 
-                        <div key={data.id} className=""> 
-                            <SubscriptionCard subscription={data} />
-                        </div>
-                    ))
-                }
+            <div className="pb-10">
+                <h1 className="text-xl md:text-2xl font-medium text-[#292929] pb-2">Subscription Packages</h1>
+                <p className="text-[#919191] text-sm font-medium">Flexible plans based on how many services you handle</p>
             </div>
-           </div>
+
+            <div>
+                <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {
+                        buySubscriptions?.map((data: any) => (
+                            <div key={data.id} className="">
+                                <SubscriptionCard subscription={data} />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </Modal>
     )
-} 
+}
 
 export default BuySubscriptionModal
